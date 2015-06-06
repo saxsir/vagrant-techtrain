20c2a9386ab6440f2aecf2a9901657cfe727bdf1
saxsir <saxsir.256+github@gmail.com>
2015-06-06 19:49:46 +0900

    [add] techtrainユーザーをsudoersに追加した
    - wheelグループはパスワードなしでsudoコマンドを使えるようにした

--- puppet/modules/os-user-2014/manifests/config.pp
+++ puppet/modules/os-user-2014/manifests/config.pp
@@ -1,7 +1,7 @@
 class os-user-2014::config{
     user { 'techtrain':
         ensure => present,
-        gid => 'developer',
+        groups => ['developer', 'wheel'],
         comment => 'techtrain',
         home => '/home/techtrain',
         password => '$6$u/a559S9y3CwTJCp$sSwn.GZDUrRGh5Ref8eKH3cxQxJHB/dCJqJOyxjnBk8zkXnasT2zLfM9LQ2EYzA.ntrwYuWrAkvXzNGYyAoLk1',
@@ -26,4 +26,16 @@ class os-user-2014::config{
         require => User["techtrain"]
     }
 
+    ssh_authorized_key { 'techtrain@techtrain':
+      user => 'techtrain',
+      type => 'ssh-rsa',
+      key  => 'AAAAB3NzaC1yc2EAAAABIwAAAQEArXF9cp9GL5XJln5byWJh+pdelI32S1R1NIWWzCt6LbNphFecgMoxPQ6Hr/9RgU9ETf3XMI+WggFbKU3pii4hCfTY573ogJz73Y7JbklkPBlWlHx9GjP2/wGfpQkUnf5Wec+v0Y0uZKxaZ8VXTZizOnXvliR8oPFEFEPMffxluCFtVyuJ7cpD0ry3HtIu4I9xcaf8x+t5zGXttqc390qOor9+nLQibpICtZlM/jfp5Z5H0wRKTUCYZHq2dTxl1wRMddOaTQtAW/z2rKXknQzIO7+p2yJTS9XPF89leLs3bci84GIp/7Gmw4HyHV5bSRwJ2lQyOkJsx0/HBmfv7d2FsQ==',
+    }
+
+    file { '/etc/sudoers':
+        owner => 'root',
+        group => 'root',
+        mode => '0440',
+        content => template('os-user-2014/sudoers'),
+    }
 }
